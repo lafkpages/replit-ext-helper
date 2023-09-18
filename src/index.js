0fcb6656a0cdc429759c81a9270272502ad0bddf
lafkpages <soy.lafk+gh@gmail.com>
2023-09-18 19:21:12 +0200

    Fix more type errors

--- src/index.js
+++ src/index.js
@@ -11,17 +11,24 @@
 // ==/UserScript==
 
 (function () {
+  const defaultAssignToQueryData = {
+    classes: [],
+    multiple: false,
+    textContent: null,
+    callback: null,
+  };
+
   /**
    * Finds elements in the document and assigns them a class.
    *
-   * @typedef {[
-   *    string | string[],
-   *    boolean,
-   *    RegExp | null,
-   *    ((elm: Element) => Element | null) | null
-   * ]} AssignToQueryData
+   * @typedef {{
+   *    classes: string | string[],
+   *    multiple: boolean,
+   *    textContent: RegExp | null,
+   *    callback: ((elm: Element) => Element | null) | null
+   * }} AssignToQueryData
    *
-   * @typedef {string | AssignToQueryData} AssignToQueryDataArg
+   * @typedef {string | Partial<AssignToQueryData>} AssignToQueryDataArg
    *
    * @param {string} query
    * A CSS query, passed to `document.querySelector` or `document.querySelectorAll`
@@ -34,14 +41,19 @@
     /**
      * @type {AssignToQueryData}
      */
-    const data = Array.isArray(_data) ? _data : [_data, false, null, null];
+    const data =
+      typeof _data == "string"
+        ? {
+            ...defaultAssignToQueryData,
+            classes: _data,
+          }
+        : { ...defaultAssignToQueryData, ..._data };
 
-    const classNames = Array.isArray(data[0]) ? data[0] : [data[0]];
-    const multiple = !!data[1];
-    const textContentRegex = data[2] instanceof RegExp ? data[2] : null;
-    const callback = data[3] instanceof Function ? data[3] : null;
+    const classNames = Array.isArray(data.classes)
+      ? data.classes
+      : [data.classes];
 
-    const elms = multiple
+    const elms = data.multiple
       ? Array.from(document.querySelectorAll(query))
       : [document.querySelector(query)];
 
@@ -52,14 +64,14 @@
 
       console.debug(elm);
 
-      if (textContentRegex) {
-        if (!elm.textContent || !textContentRegex.test(elm.textContent)) {
+      if (data.textContent) {
+        if (!elm.textContent || !data.textContent.test(elm.textContent)) {
           continue;
         }
       }
 
-      if (callback) {
-        elm = callback(elm);
+      if (data.callback) {
+        elm = data.callback(elm);
       }
 
       if (!elm) {
@@ -77,8 +89,8 @@
    * @param {Record<string, AssignToQueryDataArg>} obj
    */
   function assignToQueries(obj) {
-    for (const [query, className] of Object.entries(obj)) {
-      assignToQuery(query, className);
+    for (const [query, data] of Object.entries(obj)) {
+      assignToQuery(query, data);
     }
   }
 
@@ -86,13 +98,27 @@
     assignToQueries({
       '[data-cy="avatar-dropdown-button"]': "avatar-dropdown-btn",
       '[data-cy="header-new-repl-btn"]': "new-repl-btn",
-      button: ["new-repl-btn", true, /^\s*create\s+repl\s*$/i],
+      button: {
+        classes: "new-repl-btn",
+        multiple: true,
+        textContent: /^\s*create\s+repl\s*$/i,
+      },
       nav: "sidebar",
       ".sidebar ul": "sidebar-links",
-      ".sidebar .sidebar-links li a": ["sidebar-link", true],
+      ".sidebar .sidebar-links li a": {
+        classes: "sidebar-link",
+        multiple: true,
+      },
       ".sidebar .sidebar-bottom ul": "sidebar-bottom-links",
-      ".sidebar-bottom-links li a": ["sidebar-bottom-link", true],
-      ".sidebar button": ["sidebar-help-btn", true, /^\s*help\s*$/i],
+      ".sidebar-bottom-links li a": {
+        classes: "sidebar-bottom-link",
+        multiple: true,
+      },
+      ".sidebar button": {
+        classes: "sidebar-help-btn",
+        multiple: true,
+        textContent: /^\s*help\s*$/i,
+      },
       header: "header",
       ".header .right > div": "header-right-btns",
       ".header .header-right-btns :nth-child(2) button": "notifications-btn",
@@ -104,26 +130,32 @@
       '[data-cy="follow-button"]': "follow-btn",
       '[data-cy="feed-item-card"]': "feed-item",
       '[data-cy="repl-viewer-run-button"]': "run-repl-btn",
-      "div ~ img": ["profile-avatar", false, null, (elm) => elm.parentElement],
-      button: [
-        "copy-profile-link-btn",
-        true,
-        /^\s*copy\s+(?:profile|user)\s+(?:link|url)\s*$/i,
-      ],
+      "div ~ img": {
+        classes: "profile-avatar",
+        callback: (elm) => elm.parentElement,
+      },
+      button: {
+        classes: "copy-profile-link-btn",
+        multiple: true,
+        textContent: /^\s*copy\s+(?:profile|user)\s+(?:link|url)\s*$/i,
+      },
       '[data-cy="filetree-add-file"]': "filetree-add-file-btn",
-      '[data-cy="filetree-entity"]': ["filetree-file", true],
+      '[data-cy="filetree-entity"]': {
+        classes: "filetree-file",
+        multiple: true,
+      },
       ".invite-button": "invite-btn",
-      ".invite-btn": [
-        "deploy-btn",
-        false,
-        null,
-        (elm) =>
-          elm.parentElement.parentElement.parentElement.nextElementSibling.getElementsByTagName(
+      ".invite-btn": {
+        classes: "deploy-btn",
+        callback: (elm) =>
+          elm.parentElement?.parentElement?.parentElement?.nextElementSibling?.getElementsByTagName(
             "button"
-          )[0],
-      ],
+          )[0] ?? null,
+      },
       ".open-chat-button": "open-chat-btn",
-      '[data-cy="ws-run-btn"] button': [["run-repl-btn", "ws-run-repl-btn"]],
+      '[data-cy="ws-run-btn"] button': {
+        classes: ["run-repl-btn", "ws-run-repl-btn"],
+      },
       '[data-cy="sidebar-section-content-resources"], #sidebar-section-header-resources':
         "ws-repl-resources",
     });
