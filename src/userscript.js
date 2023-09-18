aebee07f636c4ef83c7203aa4ec775418b0b42e4
lafkpages <soy.lafk+gh@gmail.com>
2023-09-18 20:34:02 +0200

    Stuffs

---
+++ src/userscript.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Replit extension helper
+// @namespace    https://lafkpages.tech
+// @version      0.1
+// @description  Helper functions and classes for Replit extensions and userscripts
+// @author       LuisAFK
+// @match        https://replit.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=replit.com
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
