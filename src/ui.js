291e4fc3b0708f058db306a324b5d2f3e2afdbd8
lafkpages <soy.lafk+gh@gmail.com>
2023-09-19 21:14:14 +0200

    getComponentNames cache

--- src/ui.js
+++ src/ui.js
@@ -7,22 +7,33 @@ function getProjectDir() {
   return joinPaths(Bun.fileURLToPath(new URL(import.meta.url)), "../../");
 }
 
+/**
+ * @type {string[] | null}
+ */
+let componentNamesCache = null;
+
 export async function getComponentNames() {
+  if (componentNamesCache) {
+    return componentNamesCache;
+  }
+
   const projectDir = getProjectDir();
 
   const files = await readdir(
     joinPaths(projectDir, "node_modules/@replit-svelte/ui")
   );
 
-  return (
-    files
+  const names = files
 
-      // Filter out non-Svelte files.
-      .filter((file) => file.endsWith(".svelte"))
+    // Filter out non-Svelte files.
+    .filter((file) => file.endsWith(".svelte"))
 
-      // Remove the ".svelte" extension.
-      .map((file) => file.slice(0, -7))
-  );
+    // Remove the ".svelte" extension.
+    .map((file) => file.slice(0, -7));
+
+  componentNamesCache = names;
+
+  return names;
 }
 
 /**
