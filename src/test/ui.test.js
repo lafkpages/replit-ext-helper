b2352d8a5f6940366323edaee161021b05132951
lafkpages <soy.lafk+gh@gmail.com>
2023-09-19 21:12:50 +0200

    Make UI test an actual Bun test

--- src/test/ui.test.js
+++ src/test/ui.test.js
@@ -1,15 +1,16 @@
-// import { expect, test } from "bun:test";
+import { expect, test } from "bun:test";
 
 import { mkdir, rm } from "fs/promises";
 
 import { compileAllComponents } from "../ui";
 
-// test("compileComponent: Button", async () => {
-//   expect(
-//     await compileComponent("Button"),
-//   );
-// });
+test("[ui] compileAllComponents", async () => {
+  await rm("test-results/ui", { recursive: true, force: true });
+  await mkdir("test-results/ui", { recursive: true });
 
-await rm("test-results/ui", { recursive: true, force: true });
-await mkdir("test-results/ui", { recursive: true });
-await compileAllComponents("test-results/ui");
+  const components = await compileAllComponents("test-results/ui");
+
+  expect(components).toBeTruthy();
+  expect(components.Button).toBeTruthy();
+  expect(components.Button.js.code).toBeString();
+});
