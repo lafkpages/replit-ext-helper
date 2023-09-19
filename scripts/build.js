6513d038313adf76d5cac07832139c881e45236b
lafkpages <soy.lafk+gh@gmail.com>
2023-09-19 21:27:42 +0200

    Build script compile all components

--- scripts/build.js
+++ scripts/build.js
@@ -1,14 +1,22 @@
 import { rm, mkdir, copyFile } from "fs/promises";
 
+import { compileAllComponents } from "../src/ui";
+
 // Clear and create output directory.
 await rm("dist", { recursive: true, force: true });
-await mkdir("dist", { recursive: true });
+await mkdir("dist/ui", { recursive: true });
+
+// Compile all Replit Svelte components.
+await compileAllComponents("dist/ui");
 
 // Build the JS bundle.
 await Bun.build({
   entrypoints: ["src/index.js"],
   outdir: "dist",
   minify: true,
+  loader: {
+    ".svelte": "js",
+  },
 });
 
 // Copy the type definitions.
