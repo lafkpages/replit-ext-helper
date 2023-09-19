41d159a11455db6e2c930d250e7ef4ee96a97ae7
lafkpages <soy.lafk+gh@gmail.com>
2023-09-19 21:18:57 +0200

    Second getComponentNames test with Bun.peek

--- src/test/ui2.test.js
+++ src/test/ui2.test.js
@@ -9,3 +9,15 @@ test("[ui] getComponentNames", async () => {
   expect(componentNames).not.toBeEmpty();
   expect(componentNames).toContain("Button");
 });
+
+test("[ui] getComponentNames (peek)", async () => {
+  // Since getComponentNames gets cached after the first call, we can
+  // test the peek functionality by calling it twice.
+
+  const componentNamesPromise = getComponentNames();
+  const componentNamesPeek = Bun.peek(componentNamesPromise);
+
+  expect(componentNamesPeek).toBeArray();
+  expect(componentNamesPeek).not.toBeEmpty();
+  expect(componentNamesPeek).toContain("Button");
+}, 1);
