f45ab095c917744ee7314189e0238271217dfae0
lafkpages <luisafk@proton.me>
2023-11-30 17:29:13 +0100

    Remove export in declare global types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,7 +2,7 @@ import type { ReplitExtHelper } from "..";
 
 import type { NextRouter } from "next/router";
 
-export declare global {
+ declare global {
   interface Window {
     replitExtHelper: ReplitExtHelper | undefined;
     next?: {
